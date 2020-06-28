da64d1590bc18cd65f1f9a8ece829aeb6138b93d
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-06-28 02:01:52 -0300

    exercise 3

---
+++ exercise3/app.js
@@ -0,0 +1,11 @@
+const a = 5 
+const b = 6
+const c = 7
+
+const s = (a + b + c) / 2
+
+const area = Math.sqrt(s*((s-a)*(s-b)*(s-c)))
+
+document.write(area)
+
+//method Math.sqrt is used to return square root of a number 
