95e497f28ff319093453dbeb5aba641b407863ed
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-06-28 01:34:15 -0300

    exercise 2

---
+++ exercise2/app.js
@@ -0,0 +1,10 @@
+var d = new Date()
+var mm = d.getDate()
+var dd = d.getMonth()+1
+var yyyy = d.getFullYear()
+
+
+
+let today =mm+'-'+dd+'-'+yyyy+"<br>"+mm+'/'+dd+'/'+yyyy; 
+
+document.write(today)
