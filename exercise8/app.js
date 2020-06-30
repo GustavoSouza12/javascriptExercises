fa25ea21f3bce98b893b2445ce97c666552cfab1
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-06-30 02:14:44 -0300

    exercise 8

---
+++ exercise8/app.js
@@ -0,0 +1,11 @@
+function multiply(){
+     number1 = document.getElementById("number1").value
+     number2 = document.getElementById("number2").value
+    document.getElementById("result").innerHTML = number1 * number2
+}
+function divide(){
+    number1 = document.getElementById("number1").value
+    number2 = document.getElementById("number2").value
+    document.getElementById("result").innerHTML = number1 / number2
+}
+
